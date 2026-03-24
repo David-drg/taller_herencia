5147b7f97e7c752774d41386165a8fd97cd96f70
David-drg <drgarcia502@gmail.com>
2026-03-24 09:09:06 -0500

    Update primer.js

--- primer.js
+++ primer.js
@@ -1,39 +1,79 @@
+
 class Empleado {
-    constructor(nombre, cargo, salario) {
-        this.nombre = nombre;
-        this.cargo = cargo;
-        this.salario = salario;
-        this.activo = true;
-    };
-    presentarse() {
-        return `Su nombre: ${this.nombre} | cargo: ${this.cargo}`;
-    };
-    calcularSalarioAnual() {
-        return this.salario * 12;
-    };
-    desactivar() {
-        if (this.activo) {
-            this.activo = false;
-            console.log("El usuario a sido desactivado")
-        } else {
-            console.log("El usuario ya esta desactivado")
-        };
-    };
-    info() {
-        return `Nombre: ${this.nombre} | Cargo: ${this.cargo} | Salario: ${this.salario} | Estado: ${this.activo ? "Activo" : "Inactivo"}`
-    };
-};
-let usuario_1 = new Empleado("Carlos", "Developer", 3500000);
-let usuario_2 = new Empleado("julian", "Medico", 4000000);
-let usuario_3 = new Empleado("Daniela", "Ingeniera de datos", 4500000);
-//usuario1.desactivar();
-//usuario1.desactivar();
-console.log(usuario_1.presentarse());
-console.log(usuario_1.calcularSalarioAnual());
-console.log(usuario_1.info() + "\n");
-console.log(usuario_2.presentarse());
-console.log(usuario_2.calcularSalarioAnual());
-console.log(usuario_2.info() + "\n");
-console.log(usuario_3.presentarse());
-console.log(usuario_3.calcularSalarioAnual());
-console.log(usuario_3.info() + "\n");
+  constructor(nombre, cargo, salario) {
+    this.nombre = nombre;
+    this.cargo = cargo;
+    this.salario = salario;
+    this.activo = true;
+  }
+
+  presentarse() {
+    return `Hola, soy ${this.nombre} y trabajo como ${this.cargo}`;
+  }
+
+  calcularSalarioAnual() {
+    return this.salario * 12;
+  }
+
+  desactivar() {
+    this.activo = false;
+    return `${this.nombre} ha sido desactivado`;
+  }
+
+  info() {
+    return `${this.nombre} - ${this.cargo} - $${this.salario} - Activo: ${this.activo}`;
+  }
+}
+
+
+class Lider extends Empleado {
+  constructor(nombre, salario, equipo) {
+    super(nombre, "Lider de equipo", salario);
+    this.equipo = equipo;
+  }
+
+  presentarEquipo() {
+    return `Equipo: ${this.equipo.join(", ")}`;
+  }
+
+  agregarMiembro(nombre) {
+    this.equipo.push(nombre);
+    return `${nombre} agregado al equipo`;
+  }
+
+  info() {
+    return `${this.nombre} - ${this.cargo} - $${this.salario} - Equipo: ${this.equipo.length} personas - Activo: ${this.activo}`;
+  }
+}
+
+
+
+const emp1 = new Empleado("David", "Developer", 2000);
+const emp2 = new Empleado("Ana", "QA", 1800);
+const emp3 = new Empleado("Luis", "DevOps", 2200);
+const emp4 = new Empleado("Sofia", "UX", 1700);
+
+const lider1 = new Lider("Carlos", 3000, ["David", "Ana"]);
+const lider2 = new Lider("Maria", 3200, ["Luis", "Sofia"]);
+
+
+const empleados = [emp1, emp2, emp3, emp4, lider1, lider2];
+
+let totalMensual = 0;
+for (let emp of empleados) {
+  totalMensual += emp.salario;
+}
+
+console.log("Total mensual:", totalMensual);
+console.log("Total anual:", totalMensual * 12);
+
+
+emp1.desactivar();
+console.log(emp1.info());
+
+
+console.log(lider1.info());
+console.log(lider1.presentarEquipo());
+
+console.log(lider2.info());
+console.log(lider2.presentarEquipo());
